8cf05e7d1ba756ca7d4f81785f66f9aff8747034
jipsterk <17934897+JipSterk@users.noreply.github.com>
2019-01-23 16:03:29 +0100

    move to cdn

--- unity-hub/tools/chocolateyinstall.ps1
+++ unity-hub/tools/chocolateyinstall.ps1
@@ -1,8 +1,13 @@
+$ErrorActionPreference = 'Stop';
+
 $packageName = "unity-hub"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64 = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.exe"
+$checksum64 = "e539f45cc2ecd327caebba59f82ef69a83ac3a7c9b2d566a7bcecc0ed79bbcd1"
 
 $args = "/S"
+
+$pp = Get-PackageParameters
 if ($pp.InstallationPath) {
     $args += " /D=$($pp.InstallationPath)"
     Write-Host "Param: installing to $($pp.InstallationPath)"
@@ -13,6 +18,8 @@ $packageArgs = @{
     fileType       = "EXE"
     url64bit       = $url64
     softwareName   = "Unity Hub"
+    checksum64     = $checksum64
+    checksumType64 = 'sha256'
     silentArgs     = $args
     validExitCodes = @(0)
 }
--- unity-hub/tools/chocolateyuninstall.ps1
+++ unity-hub/tools/chocolateyuninstall.ps1
@@ -24,8 +24,8 @@ elseif ($key.Count -eq 0) {
     Write-Warning "$packageName has already been uninstalled by other means."
 }
 elseif ($key.Count -gt 1) {
-    Write-Warning "$key.Count matches found!"
+    Write-Warning "$($key.Count) matches found!"
     Write-Warning "to prevent accidental data loss, no prgrams will be uninstalled."
     Write-Warning "Please alert packages maintainer to following keys were matched:"
-    $key | ForEach-Object { Write-Warning "- $_.DisplayName" }
+    $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
 }
