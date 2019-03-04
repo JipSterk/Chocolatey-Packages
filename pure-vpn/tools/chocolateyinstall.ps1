eac63f86209b581a7b01d54ffa6e5f711eeaaa7f
jipsterk <17934897+JipSterk@users.noreply.github.com>
2019-03-04 15:58:56 +0100

    bump purevpn version

--- pure-vpn/tools/chocolateyinstall.ps1
+++ pure-vpn/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName = "pure-vpn"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = "https://s3.amazonaws.com/purevpn-dialer-assets/windows/app/purevpn_setup.exe"
-$checksum = "ff8bf0f341d07da58c289a80954c5627a29a6644657be03050ce661510196c7e"
+$checksum = "b4ed1dd010aecf68af30358cc32ec7e1"
 
 $args = "/S"
 
@@ -19,7 +19,7 @@ $packageArgs = @{
     url            = $url
     softwareName   = "Pure vpn"
     checksum       = $checksum
-    checksumType   = "sha256"
+    checksumType   = "md5"
     silentArgs     = $args
     validExitCodes = @(0)
 }
