a89cc56ee7792a03b6bf28c881de1a2b6cd43a39
jipsterk <17934897+JipSterk@users.noreply.github.com>
2019-05-05 22:10:23 +0200

    update sha256

--- unity-hub/tools/chocolateyinstall.ps1
+++ unity-hub/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64 = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.exe"
-$checksum64 = "514a670d8ed6267d560dc9304703fe3b30cc1113deed16eb671975f678593e1f"
+$checksum64 = "a926d9231b68398fd735f28bc5d18380254eeb8b3793c16306924495d95ddcc1"
 
 $args = "/S"
 
